81b81c7bdbb50e956e00dd9a4deec2082463cb73
valeriarc09 <valeriarengifo09@gmail.com>
2021-08-05 09:44:08 -0500

    mi primer coomit

---
+++ valeriarengifo/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          greys anatomy
+        </p>
+        <a
+          className="App-link"
+          href="https://www.youtube.com/watch?v=Zdc3shRvumk"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
